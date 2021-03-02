40dbf245ab61f4501c0fad41b99deb668a0ba1df
amie senghore <amiesenghore1@gmail.com>
2021-03-02 15:46:34 +0100

    doneExercise3js

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,17 @@
 function insert_Row() {
+    var table = document.getElementById("sampleTable");
+    var row = table.insertRow(2);
+    
+
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+
+    cell1.innerHTML="NewCell1";
+    cell2.innerHTML="NewCell2";
+
+   
+
+    
+
 
 }
